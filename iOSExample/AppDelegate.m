f60455262a8eee42a2260c32b595edeefdd16a22
Frank Schmitt <frank@apptentive.com>
2022-04-14 14:10:13 -0700

    Add push

--- iOSExample/AppDelegate.m
+++ iOSExample/AppDelegate.m
@@ -25,7 +25,31 @@
 	MParticleOptions *options = [MParticleOptions optionsWithKey:@"c0d00506599fd3498f6d74e2a6bd6784" secret:@"IdzrwE0Cd2a65-uzldce-Y3NUu1mKpuXHKt47a5yBCHxez4E7ZtBAlrUvkkGsBzR"];
 	[[MParticle sharedInstance] startWithOptions:options];
 
+    [application registerForRemoteNotifications];
+
+    [UNUserNotificationCenter.currentNotificationCenter requestAuthorizationWithOptions:UNAuthorizationOptionSound | UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            NSLog(@"Got permission!");
+        } else {
+            NSLog(@"No sounds or alerts for you! (%@)", error);
+        }
+    }];
+
 	return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[MParticle sharedInstance] didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    [[MParticle sharedInstance] didFailToRegisterForRemoteNotificationsWithError:error];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
+    [[MParticle sharedInstance] didReceiveRemoteNotification:userInfo];
+
+    completionHandler(UIBackgroundFetchResultNewData);
 }
 
 @end
